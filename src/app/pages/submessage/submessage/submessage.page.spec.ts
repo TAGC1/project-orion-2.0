30077c1641ce602eacb7069d964a3138debb6359
Jeremyah <jeremyahnunez@gmail.com>
2021-03-04 20:10:05 -0800

    Update submessage.page.spec.ts

--- src/app/pages/submessage/submessage/submessage.page.spec.ts
+++ src/app/pages/submessage/submessage/submessage.page.spec.ts
@@ -1,24 +1,58 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
-import { SubmessagePage } from './submessage.page';
-
-describe('SubmessagePage', () => {
-  let component: SubmessagePage;
-  let fixture: ComponentFixture<SubmessagePage>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SubmessagePage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(SubmessagePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { ToastController } from '@ionic/angular';
+ 
+@Component({
+  selector: 'app-submessage',
+  templateUrl: './submessage.page.html',
+  styleUrls: ['./submessage.page.scss'],
+})
+export class SubmessagePage implements OnInit {
+  message = '';
+  messages = [];
+  currentUser = '';
+ 
+  constructor(private socket: Socket, private toastCtrl: ToastController) { }
+ 
+  ngOnInit() {
+    this.socket.connect();
+ 
+    let name = `user-${new Date().getTime()}`;
+    this.currentUser = name;
+    
+    this.socket.emit('set-name', name);
+ 
+    this.socket.fromEvent('users-changed').subscribe(data => {
+      let user = data['user'];
+      if (data['event'] === 'left') {
+        this.showToast('User left: ' + user);
+      } else {
+        this.showToast('User joined: ' + user);
+      }
+    });
+ 
+    this.socket.fromEvent('message').subscribe(message => {
+      console.log('New: ', message);
+      this.messages.push(message);
+    });
+  }
+ 
+  sendMessage() {
+    this.socket.emit('send-message', { text: this.message });
+    console.log('Sent:', this.message);
+    this.message = '';
+  }
+ 
+  ionViewWillLeave() {
+    this.socket.disconnect();
+  }
+ 
+  async showToast(msg) {
+    let toast = await this.toastCtrl.create({
+      message: msg,
+      position: 'top',
+      duration: 2000
+    });
+    toast.present();
+  }
+  }
