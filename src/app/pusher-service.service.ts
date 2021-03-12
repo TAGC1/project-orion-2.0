7381eb2b5d0b2aa3cc83c5a526d5be523ce56482
Ace Software <acesoftwaregroup@gmail.com>
2021-03-12 12:49:28 -0800

    fixed pusher service

--- src/app/pusher-service.service.ts
+++ src/app/pusher-service.service.ts
@@ -1,12 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import Pusher from 'pusher-js';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PusherServiceService {
-  init(): any {
-    throw new Error('Method not implemented.');
-  }
+    @Injectable()
+    export class PusherServiceService {
+      presenceChannel;
 
-  constructor() { }
-}
+      constructor(public http: HttpClient) {
+        let pusher = new Pusher('PUSHER_APP_KEY', {
+          authEndpoint: 'http://localhost:3128/pusher/auth',
+          cluster: 'PUSHER_APP_CLUSTER'
+        });
+
+        this.presenceChannel = pusher.subscribe('presence-channel');
+      }
+
+      public init() {
+        return this.presenceChannel;
+      }
+    }
